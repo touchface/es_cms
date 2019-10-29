b9fb89f98f69afa194cd56954e58ebb26aad2b7e
touchface <572165213@qq.com>
2019-10-29 11:51:21 +0800

    添加跨域

---
+++ src/main/java/top/touchface/es_cms/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package top.touchface.es_cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.web.cors.CorsConfiguration;  
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;  
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * <p>
+ *  跨域请求配置
+ * </p>
+ *
+ * @author touchface
+ * @since 2019-10-28
+ */
+@Configuration  
+public class CorsConfig {  
+    private CorsConfiguration buildConfig() {  
+        CorsConfiguration corsConfiguration = new CorsConfiguration();  
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;  
+    }  
+  
+    @Bean  
+    public CorsFilter corsFilter() {  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);  
+    }  
+}
